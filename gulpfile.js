4d54705b0dee4b411944957ae3eee2e302d174f3
linghong <linghong_chen@hotmail.com>
2015-08-30 09:49:33 -0400

    added gulp-connect and watch, now index.html can update  automatically.

--- gulpfile.js
+++ gulpfile.js
@@ -1,1 +1,34 @@
-var gulp = require('gulp');
+var gulp = require('gulp'),
+	gutil = require('gulp-util'),
+	connect = require('gulp-connect'),
+	concat = require('gulp-concat');
+
+var htmlSources = ['builds/development/*.html'];
+var jsSources = ['components/scripts/*.js'];
+
+gulp.task('connect', function(){
+	connect.server({
+		root: 'builds/development',
+		livereload: true
+	});
+}); 
+
+gulp.task('js', function(){
+	gulp.src(jsSources)
+	.pipe(concat('script.js')
+		.on('error', gutil.log))
+	.pipe(gulp.dest('builds/development/js'))
+	.pipe(connect.reload());
+});
+
+gulp.task('html', function(){
+	gulp.src(htmlSources)
+	.pipe(connect.reload());
+});
+
+gulp.task('watch', function(){
+	gulp.watch(htmlSources, ['html']);
+	gulp.watch(jsSources, ['js']);
+});
+
+gulp.task('default', ['html','js', 'connect','watch']);
